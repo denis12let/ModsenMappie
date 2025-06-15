93ba715e1c9a4eb559b6929a44b0626a405f66d1
denis12let <84den84sokol@gmail.com>
2025-06-15 15:11:38 +0300

    feat: add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,90 @@
+import { Architecture2 } from './Archirecture2';
+import { Architecture1 } from './Architecture1';
+import { Bank } from './Bank';
+import { Bed1 } from './Bed1';
+import { Bed2 } from './Bed2';
+import { Bicycle1 } from './Bicycle1';
+import { Bicycle2 } from './Bicycle2';
+import { Car1 } from './Car1';
+import { Car2 } from './Car2';
+import { Car3 } from './Car3';
+import { Coffee } from './Coffee';
+import { Culture } from './Culture';
+import { Entertaiment1 } from './Entertaiment1';
+import { Entertaiment2 } from './Entertaiment2';
 import { Favorite } from './Favorite';
+import { Food } from './Food';
+import { Gas } from './Gas';
+import { Hint } from './Hint';
+import { History } from './History';
+import { Industrial1 } from './Industrial1';
+import { Industrial2 } from './Industrial2';
 import { Logo } from './Logo';
+import { Nature } from './Nature';
+import { Other } from './Other';
+import { Religion1 } from './Religion1';
+import { Religion2 } from './Religion2';
 import { Search } from './Search';
+import { Shop1 } from './Shop1';
+import { Shop2 } from './Shop2';
+import { Shop3 } from './Shop3';
+import { Sport1 } from './Sport1';
+import { Sport2 } from './Sport2';
+import { Sport3 } from './Sport3';
+import { Sport4 } from './Sport4';
 
 export const Icons = {
   Logo,
+
   Search,
   Favorite,
+
+  Architecture1,
+  Architecture2,
+
+  Bank,
+
+  Bed1,
+  Bed2,
+
+  Bicycle1,
+  Bicycle2,
+
+  Car1,
+  Car2,
+  Car3,
+
+  Coffee,
+
+  Culture,
+
+  Entertaiment1,
+  Entertaiment2,
+
+  Food,
+
+  Gas,
+
+  Hint,
+
+  History,
+
+  Industrial1,
+  Industrial2,
+
+  Nature,
+
+  Other,
+
+  Religion1,
+  Religion2,
+
+  Shop1,
+  Shop2,
+  Shop3,
+
+  Sport1,
+  Sport2,
+  Sport3,
+  Sport4,
 };
